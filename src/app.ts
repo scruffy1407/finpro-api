f0039fdcfb1c313fa1dbef9045cfcfc80642efac
Thomas Pratama <pratama.thomas@gmail.com>
2024-11-29 20:02:15 +0700

    first commit

---
+++ src/app.ts
@@ -0,0 +1,24 @@
+import express from "express";
+import environment from "dotenv";
+import cors from "cors";
+import { ErrorHandlerMiddleware } from "./middlewares/error.handler.middleware";
+
+environment.config();
+
+const app = express();
+const PORT = process.env.SERVER_PORT_DEV;
+const errorHandler = new ErrorHandlerMiddleware();
+
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
+
+app.use(express.json());
+
+app.use(errorHandler.errorHandler());
+
+app.listen(PORT, () => {
+  console.log(`Listening on Port : ${PORT}`);
+});
